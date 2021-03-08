42b951cc6f68e10392a93e68cfc6b76b630f50f1
Mikołaj Ryll <01142207@pw.edu.pl>
2021-03-08 22:31:27 +0100

    Add project files.

---
+++ StringCalculator.Test/CalculatorTests.cs
@@ -0,0 +1,115 @@
+using NUnit.Framework;
+using System;
+
+namespace StringCalculator.Test
+{
+    public class CalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SingleNumberReturnsValue()
+        {
+            Calculator calculator = new Calculator();
+            string input = "7";
+            int expected = 7;
+
+            int output = calculator.Add(input);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void EmptyStringReturnsZero()
+        {
+            Calculator calculator = new Calculator();
+            string input = string.Empty;
+            int expected = 0;
+
+            int output = calculator.Add(input);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void TwoNumbersCommaDelimitedReturnSum()
+        {
+            Calculator calculator = new Calculator();
+            string input = "2,9";
+            int expected = 11;
+
+            int output = calculator.Add(input);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void TwoNumbersNewlineDelimitedReturnSum()
+        {
+            Calculator calculator = new Calculator();
+            string input = "23\n15";
+            int expected = 38;
+
+            int output = calculator.Add(input);
+            Assert.AreEqual(expected, output);
+        }
+        [Test]
+        public void ThreeNumbersReturnSum()
+        {
+            Calculator calculator = new Calculator();
+            string input = "2\n9,3";
+            int expected = 14;
+
+            int output = calculator.Add(input);
+            Assert.AreEqual(expected, output);
+        }
+        [Test]
+        public void NegativeNumberThrowsException()
+        {
+            Calculator calculator = new Calculator();
+            string input = "-3";
+
+            Assert.Catch(() => calculator.Add(input));
+        }
+        [Test]
+        public void SingleCharDelimiterDefinedOnFirstLine()
+        {
+            Calculator calculator = new Calculator();
+            string input = "//#\n1#2#3";
+            int expected = 6;
+
+            int output = calculator.Add(input);
+            Assert.AreEqual(expected, output);
+        }
+        [Test]
+        public void MultiCharDelimiterDefinedOnFirstLine()
+        {
+            Calculator calculator = new Calculator();
+            string input = "//[###]\n1###2###3";
+            int expected = 6;
+
+            int output = calculator.Add(input);
+            Assert.AreEqual(expected, output);
+        }
+        [Test]
+        public void ManyMultiCharDelimiterDefinedOnFirstLine()
+        {
+            Calculator calculator = new Calculator();
+            string input = "//[###][#][eee]\n1#2###3eee4";
+            int expected = 10;
+
+            int output = calculator.Add(input);
+            Assert.AreEqual(expected, output);
+        }
+        [Test]
+        public void SuperComplicatedTest()
+        {
+            Calculator calculator = new Calculator();
+            string input = "//[###][!!][Maciek][#][eee][ ]\n1#1###1eee1!!1Maciek1 1";
+            int expected = 7;
+
+            int output = calculator.Add(input);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        private static string[] separators = new string[]
+        {
+            ",",
+            "\n"
+        };
+
+        public int Add(string input)
+        {
+            string[] usedSeparators = separators;
+            string validInput = input;
+
+            if (string.IsNullOrEmpty(input))
+                return 0;
+
+            if (input.Length > 2)
+                if (input[0] == '/' && input[1] == '/')
+                {
+                    int index = input.IndexOf('\n');
+                    validInput = input.Substring(index + 1);
+                    if (input[2] == '[')
+                    {
+                        string[] declaredSeparatos = input.Split('[',']');
+                        List<string> readSeparators = new List<string>();
+                        for (int i = 1; i < declaredSeparatos.Length; i++)
+                            if (string.IsNullOrEmpty(declaredSeparatos[i]))
+                                continue;
+                            else if (declaredSeparatos[i][0] == '\n')
+                                break;
+                            else
+                                readSeparators.Add(declaredSeparatos[i]);
+                        usedSeparators = readSeparators.ToArray();
+                    }
+                    else if (input[3] == '\n')
+                    {
+                        usedSeparators = new string[] { input[2].ToString() };
+                    }
+                }
+
+            string[] values = validInput.Split(usedSeparators, StringSplitOptions.None);
+            int firstValue = int.Parse(values[0]);
+            int value = firstValue > 1000 ? 0 : firstValue;
+            if (value < 0) throw new Exception();
+            for (int i = 1; i < values.Length; i++)
+            {
+                int newValue = int.Parse(values[i]);
+                if (newValue < 0) throw new Exception();
+                value += newValue > 1000 ? 0 : newValue;
+            }
+
+            return value;
+        }
+    }
+}
